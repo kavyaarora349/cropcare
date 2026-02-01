4e12b67f82fba6fa5e5e5c5c53678f75c97ccc71
Kavya Arora <kavyaarora349@gmail.com>
2026-02-02 01:08:10 +0530

    Fix CORS to allow deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -47,6 +47,7 @@ app.add_middleware(
         "http://127.0.0.1:8082",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://cropcare-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
